3a6f76b63f0a2734a8480e8916b40f3500510e58
Vinicius Kobs <viniciusrehbeinkobs@gmail.com>
2023-08-24 07:32:06 -0300

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { exampleRoute, enginesRoute } from './routes';
 import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 app.use('/', exampleRoute);
 app.use('/engines', enginesRoute);
